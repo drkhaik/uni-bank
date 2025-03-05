0c5fe54270f8341e8fda02dbd5329ddc307e3cf0
drkhaik <trinhkhaiviet@gmail.com>
2025-03-05 18:08:39 +0700

    Add application-example.properties, jwt service for authentication

---
+++ src/main/java/unibank/web/app/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package unibank.web.app.config;
+
+public class SecurityConfig {
+}
--- src/main/java/unibank/web/app/entity/User.java
+++ src/main/java/unibank/web/app/entity/User.java
@@ -4,9 +4,15 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Builder
@@ -14,18 +20,19 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "bank_user")
-public class User {
+@Table(name="bank_user")
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String uid;
 
-    private String firstName;
-    private String lastName;
+    private String firstname;
+    private String lastname;
 
     @Column(nullable = false, unique = true)
-    private String userName;
+    private String username;
+
     private Date dob;
     private long tel;
     private String tag;
@@ -47,5 +54,10 @@ public class User {
 
     @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Account> accounts;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
 }
 
